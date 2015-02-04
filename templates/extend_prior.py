0bb01fe1fab310dfc7d9c500463a91641b751687
Gabe Brammer <gbrammer@users.noreply.github.com>
2015-02-04 16:32:45 +0000

    Fixed bugs in R_extend prior and extrapolation to bright/faint magnitudes.  N.B.: R prior has probability at z>4 where should probably go to zero for IGM absorption

--- templates/extend_prior.py
+++ templates/extend_prior.py
@@ -34,13 +34,25 @@ def go(mags=(15,30.1,0.5), redshifts=(0.01, 12, 0.01), coeff_file='prior_K_zmax7
     NM = mag_grid.shape[0]
     
     #### Polynomial extrapolation not reliable
-    # p_z0 = scipy.polyfit(mag_list, z0, 3) 
+    # p_z0 = scipy.polyfit(mag_list, z0, order) 
     # z0_grid = np.maximum(scipy.polyval(p_z0, mag_grid), 0.05)
-    # p_gamma = scipy.polyfit(mag_list, gamma, 3) 
+    # p_gamma = scipy.polyfit(mag_list, gamma, order) 
     # gamma_grid = np.maximum(scipy.polyval(p_gamma, mag_grid), 0.05)
     
+    #### Interpolations on the defined grid
     z0_grid = np.interp(mag_grid, mag_list, z0)
     gamma_grid = np.interp(mag_grid, mag_list, gamma)
+    
+    #### Linear extrapolations of fit coefficients
+    p_z0 = scipy.polyfit(mag_list[:3], z0[:3], 1)
+    z0_grid[mag_grid < mag_list[0]] = np.maximum(scipy.polyval(p_z0, mag_grid[mag_grid < mag_list[0]]), 0.05)
+    p_z0 = scipy.polyfit(mag_list[-3:], z0[-3:], 1)
+    z0_grid[mag_grid > mag_list[-1]] = np.maximum(scipy.polyval(p_z0, mag_grid[mag_grid > mag_list[-1]]), 0.05)
+
+    p_gamma = scipy.polyfit(mag_list[:3], gamma[:3], 1)
+    gamma_grid[mag_grid < mag_list[0]] = np.maximum(scipy.polyval(p_gamma, mag_grid[mag_grid < mag_list[0]]), 0.05)
+    p_gamma = scipy.polyfit(mag_list[-3:], gamma[-3:], 1)
+    gamma_grid[mag_grid > mag_list[-1]] = np.maximum(scipy.polyval(p_gamma, mag_grid[mag_grid > mag_list[-1]]), 0.05)
     
     out_matrix = np.zeros((NZ, NM+1))
     out_matrix[:,0] = z_grid
@@ -48,9 +60,11 @@ def go(mags=(15,30.1,0.5), redshifts=(0.01, 12, 0.01), coeff_file='prior_K_zmax7
     for i in range(NM):
         pz = z_grid * np.exp(-(z_grid/z0_grid[i])**gamma_grid[i])
         pz /= np.trapz(pz, z_grid)
-        plt.plot(z_grid, pz)
+        plt.plot(z_grid, pz, label=mag_grid[i])
         out_matrix[:,i+1] = pz
-        
+    
+    plt.legend(ncol=3, loc='upper right')
+    
     header = '# z '
     for m in mag_grid: 
         header += '%6.1f' %(m)
