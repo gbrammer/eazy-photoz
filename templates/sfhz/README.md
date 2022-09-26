f8b84a20f8e781d1f8244a24cd347a24a40f1558
Gabriel Brammer <gbrammer@gmail.com>
2022-09-26 13:02:14 +0200

    add sfhz parameters figure

--- templates/sfhz/README.md
+++ templates/sfhz/README.md
@@ -3,6 +3,10 @@ than the age of the universe at a given epoch.
 
 <img src=https://github.com/gbrammer/eazy-photoz/blob/master/templates/sfhz/corr_sfhz_zdependence.png width=60%/>
 
+The maximum attenuation of the reddened templates also evolves with redshift.
+
+<img src=https://github.com/gbrammer/eazy-photoz/blob/master/templates/sfhz/corr_sfhz_parameters.png width=60%/>
+
 `4590.fits` is the best-fit template to the extreme emission line galaxy
 at z=8.5 with a NIRSpec spectrum, provided by A. Carnall, rescaled to roughly
 match the normalization of the FSPS templates.
--- templates/sfhz/show.py
+++ templates/sfhz/show.py
@@ -37,3 +37,53 @@ for i, z in enumerate([0.1, 3., 8.]):
 fig.tight_layout(pad=1)
 
 fig.savefig('corr_sfhz_zdependence.png')
+
+# Parameter variation with redshift
+from grizli import utils
+from astropy.cosmology import WMAP9
+
+templ = utils.read_catalog('corr_sfhz_13_bin0_av0.01.fits')
+zstep = []
+for k in templ.meta:
+    if k.startswith('Z') & (k not in ['ZMET','ZRED']):
+        print(k)
+        zstep.append(templ.meta[k])
+
+par = utils.read_catalog('corr_sfhz_13.param.fits')
+par[r'$\log_{10}$ sSFR'] = np.log10(par['sfr']/par['mass'])
+par[r'$M/L_{V}$'] = par['mass']/par['Lv']
+par[r'$L_\mathrm{OIII} / L_{V}$'] = par['LOIII']/par['Lv']
+par[r'$A_V$'] = par['Av']
+
+keys = ['lwAgeV',r'$A_V$',r'$\log_{10}$ sSFR',r'$M/L_{V}$',
+        r'$L_\mathrm{OIII} / L_{V}$']
+
+fig, axes = plt.subplots(len(keys),1,figsize=(8,8), sharex=True)
+for i, k in enumerate(keys):
+    axes[i].set_ylabel(k)
+    axes[i].grid()
+    
+    for j in range(len(par)-1):
+        axes[i].plot(np.log(1+np.array(zstep)), par[k][j], marker='.', alpha=0.5)
+
+for i in [0, 3, 4]:
+    axes[i].semilogy()
+        
+axes[i].set_xlabel('z')
+xt = np.arange(0,12.1,2)
+
+axes[i].set_xticklabels(xt)
+axes[i].set_xticks(np.log(1+xt))
+
+axes[0].plot(np.log(1+np.array(zstep)), WMAP9.age(zstep), color='0.8', marker='.', label='WMAP9')
+axes[0].legend(loc='upper right')
+
+axes[0].set_ylim(0.008, 14)
+
+axes[0].set_yticks([0.01, 0.1, 1])
+axes[0].set_yticklabels(['10 Myr', '100', '1 Gyr'])
+
+fig.tight_layout(pad=1)
+
+fig.savefig('corr_sfhz_parameters.png')
+
